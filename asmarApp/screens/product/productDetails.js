31bd521c2e970dc47db8dea5246e12fc3486e7bf
BrunaLins <51920797+BrunaLins@users.noreply.github.com>
2022-01-26 21:43:08 +0100

    produit page version mobile

--- asmarApp/screens/product/productDetails.js
+++ asmarApp/screens/product/productDetails.js
@@ -1,59 +1,122 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from 'react';
 import { productService } from '../../services/productService';
-import { ScreenContainer } from "react-native-screens";
 import { Button } from 'react-native-paper';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {StyleSheet, Text, View, Image,SafeAreaView, Dimensions }from 'react-native';
 
-export default function ProductScreen({ navigation }) {
-    const [qty, setQty] = useState(1);
-    const [productDetails, setProductDetails] = useState({});
-    const [urlImages, setUrlImages]= useState([]);
-    const [imagePrincipal,setImagePrincipal] =useState(null);
-    const [noImagePrincipal, setNoImagePrincipal] = useState([]);
+export default function ProductScreen() {
 
-    let ajouterPanier = () => {
-    }
+        const [qty, setQty] = useState(1);
+        const [productDetails, setProductDetails] = useState({});
+        const [urlImages, setUrlImages]= useState([]);
+        const [imagePrincipal,setImagePrincipal] =useState(null);
+        const [noImagePrincipal, setNoImagePrincipal] = useState([]);
 
-    const getProductDetailsById = () => {
-        productService
-            .getProductDetailsById(1)
-            .then((res) => {
-                setProductDetails(res.data);
-                setUrlImages(res.data.urlImages);
-                let imgTab =[];
-                let firstImg = null;
-                for( let item of res.data.urlImages){
-                    console.log(item);
-                    imgTab.push(item.url);
-                    console.log(imgTab);
 
-                    if (item.principal){
-                        firstImg = item.url;
-                        setImagePrincipal(firstImg);
-                        console.log(firstImg);
-                    }
+          const getProductDetailsById = () => {
+            productService
+                .getProductDetailsById(1)
+                .then((res) => {
+                    setProductDetails(res.data);
+                    setUrlImages(res.data.urlImages);
+                    let imgTab =[];
+                    let firstImg = null;
+                    for( let item of res.data.urlImages){
+                        console.log(item);
+                        imgTab.push(item.url);
+                        console.log(imgTab);
 
-                };
-                setNoImagePrincipal(imgTab) ;
+                        if (item.principal){
+                            firstImg = item.url;
+                            setImagePrincipal(firstImg);
+                            console.log(firstImg);
+                        }
+                    };
+                    setNoImagePrincipal(imgTab) ;
 
-                console.log(res.data);
-            })
-            .catch(err => console.log(err))
-    }
-    useEffect(() => {
-        getProductDetailsById();
-    }, []);
+                    console.log(res.data);
+                })
+                .catch(err => console.log(err))
+        }
+        useEffect(() => {
+            getProductDetailsById();
+        }, []);
 
-    return(
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
-            <Text style={{ fontSize: 26, fontWeight: "bold"}}>Product Screen (Cheyu)</Text>
-        </View>
-    );
+    return (
 
-}
+        <SafeAreaView style={styles.container}>
+                    <View style={{flexDirection:"row"}}>
+                        <View style={styles.images}>
+                            {noImagePrincipal.map((img, index)=>(
+                                <Image source={img} key={index} style={{ width: '100%', height: 400/noImagePrincipal.length}} alt="productImage"/>
+                            ))}
+                        </View>
+                       <View style={styles.imagePrincipal}>
+                            <Image source={imagePrincipal}style={{height: 400+ 5*(noImagePrincipal.length-1), width: '100%'}} alt="principal"/>
+                        </View>
+                    </View>
 
-export default ProductScreen;
+                    <Text style={styles.text}>{productDetails.label}</Text>
+                                <Text style={styles.text}>Origin : {productDetails.origin}</Text>
+                        <Text style={styles.text}>Material : {productDetails.composition}</Text>
+                            <Text style={styles.text}>{productDetails.usage_ && (<Text>Model d'emploi : {productDetails.usage_}</Text>)}
+                            </Text>
+                        <Text  style={styles.text}>{productDetails.price} € </Text>
+                            <Text style={{color: productDetails.quantity > 0 ? 'green' : 'red'}}>
+                                Status :{''}
+                                {productDetails.quantity > 0 ? 'disponible' : 'épuisé'}
+                            </Text>
 
-const styles = StyleSheet.create({});
+                    <Text style={{color: '#003B49'}}>
+                        Quantité :{''}
+                        <select data={qty}
+                                onChange={(e) => {
+                                    setQty(e.target.value);
+                                }
+                                } style={{width:100, borderRadius:2, marginLeft:40}} >
+                            {[...Array(productDetails.quantity).keys()].map((x) => (
+                                <option key={x + 1} value={x + 1}> {x + 1} </option>))}
+                        </select>
+
+                            {productDetails.quantity > 0 && (
+                                <Button icon="cart" mode="contained" style={{width: '110px', height: '25px', backgroundColor:
+                                    '#003B49',marginLeft: '20px'}} labelStyle={{color:'#F4D19E' , fontSize: '10px',marginTop:'7px'}} onPress={() => console.log('Pressed')}
+                                        >
+                                    Ajouter
+                                </Button>)}
+                    </Text>
+
+            <Text  style={styles.text}>{productDetails.description && (<Text> Description : {productDetails.description}</Text>)}
+            </Text>
+
+            <Text  style={styles.text}>{productDetails.history && (<Text> Histoire : {productDetails.history}</Text>)}
+            </Text>
+        </SafeAreaView>
+        )
+};
+
+const styles = StyleSheet.create({
+
+    container: {
+        backgroundColor: '#fff',
+        flexDirection: "column",
+                        margin:0,
+                        flex: 1,
+    },
+    text: {
+        color:'#003B49',
+        fontWeight: 'bold',
+    },
+    imagesContainer:{
+        padding: 0,
+    },
+    images:{
+    height: 65,
+    width: '30%' ,
+    alignItems:'center',
+    gap: 5,
+    },
+    imagePrincipal :{
+    width: '70%',
+    marginLeft: 5,
+    },
+});
--- asmarApp/services/productService.js
+++ asmarApp/services/productService.js
@@ -2,14 +2,17 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8080/api/products';
 
+/*function getProductDetailsById(id) {
+    return axios.get(BASE_URL+'/'+id);
+}*/
 function getProductDetailsById(id) {
     return axios.get(BASE_URL+'/'+id);
 }
+
 
 function getProductsByCategoryId(categoryId) {
     return axios.get(BASE_URL+'/'+categoryId);
 }
 export const productService = {
-    getProductDetailsById,
-    getProductsByCategoryId
+    getProductDetailsById
 };
