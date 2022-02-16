823a54ae640334cbdd76ae858f843a5b3eee3c9b
BrunaLins <51920797+BrunaLins@users.noreply.github.com>
2022-02-16 20:22:17 +0100

    create category page

--- asmarweb/src/screens/categories/categories.js
+++ asmarweb/src/screens/categories/categories.js
@@ -1,12 +1,37 @@
-import * as React from "react";
+import Row from 'react-bootstrap/Row';
+import React, { useEffect, useState} from 'react';
+import {productService} from '../../services/productService';
+import ProductItem from "../../components/ProductItem";
 
 
 export default function CategoriesScreen() {
-    return (
-        <div>
-            <div>Catégories Screen</div>
-            <p>Catégories page</p>
-        </div>
-    );
 
-}
+    const [products, setProducts] = useState([]);
+
+    const getProducts = () => {
+        productService
+            .getProductsByCategoryId(2)
+            .then((res) => {
+                setProducts(res.data);
+                console.log(res.data);
+            })
+            .catch(err => console.log(err))
+    }
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+
+    return (
+
+        <div className='row-wrapper'>
+            <Row>
+                {products.map(product => (
+                    <ProductItem key={product.id} product={product} />
+                ))}
+            </Row>
+        </div>
+    )
+
+};
+
