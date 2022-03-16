bbe63770d5a567971e201a83ad0c209c294bfdc8
ABDELFADEIL <abdalfadeil@gmail.com>
2022-03-16 17:37:55 +0100

    modif

--- asmar-backend/src/main/java/com/aston/ecommerce/asmar/controller/CategoryController.java
+++ asmar-backend/src/main/java/com/aston/ecommerce/asmar/controller/CategoryController.java
@@ -19,7 +19,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
-
     private final CategoryService categoryService;
     private final CategoryRepository categoryRepository;
 
