6e631812b857eccbb50f8001a15a8e5d649fd318
BrunaLins <51920797+BrunaLins@users.noreply.github.com>
2022-04-11 16:42:33 +0200

    search page without design part

--- asmarApp/utils/constants.js
+++ asmarApp/utils/constants.js
@@ -1,4 +1,4 @@
 import React from 'react';
 
-export const BASE_URL = 'https://9274-77-132-233-216.ngrok.io'
+export const BASE_URL = 'https://aa83-178-208-9-100.ngrok.io'
 // 127.0.0.1 // 192.168.1.28 // 192.168.196.14
--- asmarweb/src/components/header/Navbar.js
+++ asmarweb/src/components/header/Navbar.js
@@ -40,7 +40,7 @@ export default class Navbar extends Component{
                         <li>VÊTEMENTS</li>
                     </Link>
 
-                    <Link to="/product-details" onClick={this.handleClick}>
+                    <Link to="/search" onClick={this.handleClick}>
                         <li><FaSearch /></li>
                     </Link>
                     <Link to="/login" onClick={this.handleClick}>
